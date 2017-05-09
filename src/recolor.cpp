02e7caf960ae5c79b80bb3d96024ff420022fff6
Tina <tina.han.tiantian@gmail.com>
2017-05-09 13:29:10 -0400

    added functionality for accepting mask for getting average color, generated piecewise curve preliminary result

--- src/recolor.cpp
+++ src/recolor.cpp
@@ -11,16 +11,14 @@ using namespace cv;
  */
 int main( int argc, char** argv )
 {
-    /// Load the source image
-    Mat src = imread( argv[1], IMREAD_COLOR);
-
-	String usage = "usage: recolor input_file_path param1 param2 param3\n"
+	String usage = "usage: recolor input_file_path param1 param2 param3 file_path_to_mask\n"
 			"    input_file_path: path from current location to input image\n"
-			"    param1: average R\n"
+			"    param1: average B\n"
 			"    param2: average G\n"
-			"    param3: average B\n"
+			"    param3: average R\n"
+			"    param4: file_path_to_mask (optional)\n"
 			"    output: saves output image to output folder\n";
-    if(argc < 4){
+    if(argc < 5){
     	printf("%s", usage.c_str()); cout.flush();
     	return 0;
     }
@@ -31,10 +29,32 @@ int main( int argc, char** argv )
     	input_params(i - 2) = atof(argv[i]);
     }
 
+    /// Load the source image
+    printf("DEBUG: image name name %s\n", argv[1]);
+    Mat src = imread( argv[1], IMREAD_COLOR); //reads 3 channel BGR
+
     // check input
-    if(src.size().width <= 0 || src.size().height <= 0){
-    	perror("ERROR: check input, image size < 0");
+    if(src.size().width <= 20 || src.size().height <= 20){
+    	perror("ERROR: check input, image size should be at least 20 x 20 \n");
     	return 1;
+    }
+
+	printf("DEBUG: loaded image  w = %d h = %d\n", src.size().width, src.size().height); cout.flush();
+
+    // get mask if provided
+    Mat average_col_mask;
+    bool has_mask = false;
+    if(argc >= 6){
+    	printf("DEBUG: mask name %s\n", argv[5]);
+    	average_col_mask = imread(argv[5], IMREAD_GRAYSCALE);
+
+    	if(average_col_mask.size().width != src.size().width || average_col_mask.size().height != src.size().height){
+    		perror("ERROR: check input, mask size doesn't match image size\n");
+        	return 1;
+        }
+
+    	printf("DEBUG: loaded image mask w = %d h = %d\n", average_col_mask.size().width, average_col_mask.size().height); cout.flush();
+    	has_mask = true;
     }
 
     /*double alpha = input_params(0);
@@ -56,8 +76,13 @@ int main( int argc, char** argv )
 
     // calculations
     Scalar target = Scalar(input_params(0), input_params(1), input_params(2));
-    Mat average_roi = src( Rect(src.rows/2 - 5,src.rows/2 + 5,10,10) );
-    Scalar average = cv::mean(average_roi);
+    Scalar average;
+    if(has_mask){
+    	average = mean(src, average_col_mask);
+    } else {
+		Mat average_roi = src( Rect(src.rows/2 - 5,src.rows/2 + 5,10,10) );
+		average = mean(average_roi);
+	}
     printf("DEBUG: image average b = %.2f, g = %.2f, r =%.2f\n", average(0), average(1), average(2)); cout.flush();
 
     // process
@@ -81,8 +106,17 @@ int main( int argc, char** argv )
     	}
     }
 
+    //DEBUG, draw average colors
+    rectangle(dst, Point(dst.cols - 7, dst.rows - 14), Point(dst.cols - 2, dst.rows - 9), average, CV_FILLED);
+    rectangle(dst, Point(dst.cols - 7, dst.rows - 7), Point(dst.cols - 2, dst.rows - 2), target, CV_FILLED);
+
     // print output
-    imwrite("outputs/small_range.jpg", dst);
+    imwrite("outputs/output.jpg", dst);
+
+    //DEBUG
+    if(has_mask){
+    	imwrite("outputs/got_mask.jpg", average_col_mask);
+    }
 
     return 0;
 }
