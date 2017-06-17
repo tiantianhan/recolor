22e7793350d1f9e3ba22fed6c3a91ef71fae607e
Tina <tina.han.tiantian@gmail.com>
2017-06-17 14:19:31 -0400

    added comparison algorithm skeleton files; writing: lit review: reinhard algo description

---
+++ src/algo_reinhard.cpp
@@ -0,0 +1,17 @@
+/*
+ * algo_reinhard.cpp
+ *
+ *  Created on: Jun 17, 2017
+ *      Author: tina
+ */
+#include "algo_reinhard.hpp"
+
+using namespace std;
+using namespace cv;
+
+
+int piecewise_process(cv::Mat src, cv::Mat dst){
+
+	return 0;
+}
+
---
+++ src/algo_reinhard.hpp
@@ -0,0 +1,20 @@
+/*
+ * algo_reinhard.hpp
+ *
+ *  Created on: Jun 17, 2017
+ *      Author: tina
+ */
+
+#ifndef ALGO_REINHARD_HPP_
+#define ALGO_REINHARD_HPP_
+
+#include <iostream>
+#include "opencv/cv.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "algo_piecewise.hpp"
+
+int piecewise_process(cv::Mat src, cv::Mat dst);
+
+
+#endif /* ALGO_REINHARD_HPP_ */
